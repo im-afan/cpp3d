f1611d24efbaafda4c1fdb42fc5316b9b7b49b2a
andrew <andrewkongfan@gmail.com>
2022-10-10 21:51:03 -0700

    rotating triangle

--- src/engine/terminalCanvas.h
+++ src/engine/terminalCanvas.h
@@ -3,26 +3,31 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <Eigen/Dense>
 
 class TerminalCanvas {
 public:
 	int width, height;
 	//int** state;
 	std::vector<std::vector<int>> state;
+	std::vector<std::vector<double>> zbuffer;
 	std::string ramp = ".-:=+m&$#";
 	
 	TerminalCanvas(int w, int h){ //width, height
 		width = w, height = h;
 		
 		std::vector<int> temp(width);
+		std::vector<double> temp1(width);
 		std::fill(temp.begin(), temp.end(), 0);
+		std::fill(temp1.begin(), temp1.end(), 0);
 
 		for(int i = 0; i < height; i++){
 			state.push_back(temp);
+			zbuffer.push_back(temp1);
 		}		
 	}
 
-	void drawPoint(double x, double y, double a){ // 0 <= x, y <= 1
+	void drawPoint(double x, double y, double a){ // 0 <= x, y <= 1 TODO: switch to vector input
 		if(0 <= x && x <= 1 && 0 <= y && y <= 1)
 			state[(int) (y*height)][(int) (x*width)] = (int) (a*(ramp.size()-1));
 	}
@@ -31,6 +36,7 @@ public:
 		for(int i = 0; i < height; i++){
 			for(int j = 0; j < width; j++){
 				state[i][j] = 0;
+				zbuffer[i][j] = 0;
 			}
 		}
 	}
@@ -53,10 +59,12 @@ public:
 		//std::cout << ret << std::endl;
 	}
 
-	void drawSegment(double x1, double y1, double x2, double y2, double a){
+	void drawSegment(double x1, double y1, double x2, double y2, double a){ //TODO: switch to vector input
 		for(double i = 0; i < 1; i += 0.01){
 			drawPoint(i*x1+(1-i)*x2, i*y1+(1-i)*y2, a);
 		}
 	}
+
+	void drawTriangle(Eigen::VectorXd p1, Eigen::VectorXd p2, Eigen::VectorXd p3, double a1, double a2, double a3){}
 };
 
