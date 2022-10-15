2ce3f223d00d1b1fa7e73868a8c2a3d8140543db
Andrew Fan <andrew@LAPTOP-ES22F19F.localdomain>
2022-10-14 21:02:46 -0700

    3d works

--- src/engine/perspectiveCamera.h
+++ src/engine/perspectiveCamera.h
@@ -8,28 +8,39 @@ class PerspectiveCamera{
 		double right, left, top, bottom;
 		double near, far;
 		
-		Eigen::Vec4d pos = {0.0, 0.0, 0.0, 1.0};
-		Eigen::Vec4d eulerAngles = {0.0, 0.0, 0.0};
+		Eigen::Vector4d pos = {0.0, 0.0, 0.0, 1.0};
+		Eigen::Vector4d eulerAngles = {0.0, 0.0, 0.0, 0};
 
-		PerspectiveCamera(left, right, bottom, top, near, far){
-			right = right;
-			left = left;
-			bottom = bottom;
-			top = top;
-			near = near;
-			far = far;
+		PerspectiveCamera(double l, double r, double b, double t, double n, double f){
+			right = r;
+			left = l;
+			bottom = b;
+			top = t;
+			near = n;
+			far = f;
 		}
 
-		Eigen::Vec2d project(Eigen::Vec4d point){
-			
-			
+		Eigen::Vector2d project(Eigen::Vector4d point){
 			//view matrix
-			Eigen::Mat4d rot = rotation3d(-eulerAngles);
-			Eigen::Mat4d transform = rotation3d(-pos);	
+			//printf("point before view matrix: %f %f %f\n", point[0], point[1], point[2]);
+			Eigen::Matrix4d rot = rotation3d(-eulerAngles);
+			Eigen::Matrix4d transform = rotation3d(-pos);	
+			//std::cout << "view matrix: " << rot*transform << std::endl;
 			point = rot * transform * point;
-			double x = point[0], y = point[1], z = point[2];		
-	
+			
+			double x = point[0], y = point[1], z = point[2];
+
+			//if(z > far || z < near)
+			//	return Eigen::Vector2d(-2, -2);		
+			//printf("point after view matrix: %f %f %f\n", x, y, z);
 			//draw a "line" to the projection plane
-			return Eigen::Vec2d proj(near/z*x * 1/(right-left), near/z*y * 1/(top-bottom)); //reminder: it's from -1 to 1, not 0 to 1 rn
+			//printf("near: %f, right: %f, left: %f\n", near, right, left);
+			//printf("near/z*x: %f, 1/(right-left): %f\n", near/z*x, 1/right-left);
+			if(z >= near) 
+				return Eigen::Vector2d(near/z*x * 1/(right-left), near/z*y * 1/(top-bottom)); //reminder: it's from -1 to 1, not 0 to 1 rn
+			else
+				return Eigen::Vector2d(near/z*x * 1/(right-left), near/z*y * 1/(top-bottom)); //reminder: it's from -1 to 1, not 0 to 1 rn
+			//return Eigen::Vector2d(near/z*x, near/z*y);
+			//return Eigen::Vector2d(0.5, 0.5);
 		}
-}
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,71 @@
 #pragma warning (push)
+#pragma once
 
 #include <iostream>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <Eigen/Dense>
-#include "engine/geometry.h"
+//#include "engine/geometry.h"
 #include "engine/terminalCanvas.h"
+#include "engine/perspectiveCamera.h"
+#include <vector>
 
 int main(int argc, char* argv[]){
 	std::ios_base::sync_with_stdio(false);
-	Eigen::Vector3d vec(0, 0, 0.1);
 	
-	Eigen::Vector4d pos1(0.5, 0, 0, 1);
+	/*Eigen::Vector4d pos1(0.5, 0, 0, 1);
 	Eigen::Vector4d pos2(0, 0.5, 0, 1);
-	Eigen::Vector4d pos3(0.5, 0.5, 0, 1);
+	Eigen::Vector4d pos3(0.5, 0.5, 0, 1);*/
+	std::vector<Eigen::Vector4d> verts = {
+		Eigen::Vector4d(1, 1, 0, 1),
+		Eigen::Vector4d(-1, 1, 0, 1),
+		Eigen::Vector4d(-1, -1, 0, 1),
+		Eigen::Vector4d(1, -1, 0, 1)
+	};
+
+	std::vector<int> inds = {
+		0, 1, 2,
+		0, 3, 2
+	};
 	
+	PerspectiveCamera cam(-1, 1, -1, 1, 1, 100);
+	
+	Eigen::Vector3d vec(0.01, 0, 0.01);
 	Eigen::Matrix4d rotmat = rotation3d(vec);
+
+	Eigen::Matrix4d transform = translation3d(Eigen::Vector3d(0, 0, 2)); //rename this to model matrix later oops
 
 	std::cout << rotmat << std::endl;
 
-	int width = 160, height = 44;
+	int width = 160, height = 60;
 
 	TerminalCanvas canvas(width, height);
 
 	char* s;
 
 	while(true){
-		canvas.clear();
-		pos1 = rotmat * pos1;
-		pos2 = rotmat * pos2;
-		pos3 = rotmat * pos3;
+		canvas.clear(); //reset canvas
 		
-		canvas.drawTriangle(pos1, pos2, pos3, 1, 0.5, 0.1);
-
+		/*for(int i = 0; i < verts.size(); i++) {
+			verts[i] = rotmat * verts[i]; //apply rotation to all vertices
+		}*/
+		transform = transform * rotmat;
+		
+		for(int i = 0; i < inds.size(); i += 3){ //draw each triangle
+			int i1 = inds[i], i2 = inds[i+1], i3 = inds[i+2];
+			//get projected points using camera
+			Eigen::Vector2d proj1 = cam.project(transform * verts[i1]), proj2 = cam.project(transform * verts[i2]), proj3 = cam.project(transform * verts[i3]);
+			
+			//projected points are between 1 and -1, but we need them between 0 and 1 to draw them
+			Eigen::Vector2d move(1, 1);
+			canvas.drawTriangle((proj1+move)/2, (proj2+move)/2, (proj3+move)/2, 1, 0.75, 0.5);
+		}
+		
 		s = canvas.render();
 		
 		printf("\033[H\033[2J");
 		printf("%s", s);
-		//std::cout << s;
 		usleep(8000*3);
 	}
 	
